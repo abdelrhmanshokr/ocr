2eb177aa6bac58e7760d3a3e9ceb92fb38e68e73
abdelrhmanshokr <abdelrhmanshokr13@gmail.com>
2021-06-19 16:45:21 +0200

    first commit

---
+++ index.py
@@ -0,0 +1,82 @@
+from matplotlib import pyplot as plt
+from pytesseract import Output
+import cv2, pytesseract, re, json
+import numpy as np
+import imutils
+
+# reading an image
+image = cv2.imread('test.jpeg')
+
+# image preprocessing 
+# image resizing 
+# gray scale
+gray_scale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# apply adaptive threshold
+preprocessed_image = cv2.adaptiveThreshold(gray_scale_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 91, 3)
+
+# configuring parameters for the tesseract with the image
+custom_config = r'--oem 3 --psm 6'
+
+# feeding the image to the tesseract 
+details = pytesseract.image_to_data(image, output_type=Output.DICT, \
+                        config=custom_config)
+
+preprocessed_details = pytesseract.image_to_data(preprocessed_image, output_type=Output.DICT, \
+                        config=custom_config)
+
+date_regs = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
+barcode_regex = '^\d{12}$'
+three_digit_scratch_code_regex = '^\d{3}$'
+four_digit_scratch_code_regex = '^\d{4}$'
+
+print('details', details['text'])
+print('pre processed details', preprocessed_details['text'])
+total_boxes = len(details['text'])
+total_adaptive_boxes = len(details['text'])
+for sequence_number in range(total_boxes):
+    if re.match(date_regs, details['text'][sequence_number]) or re.match(barcode_regex, details['text'][sequence_number]) or re.match(three_digit_scratch_code_regex, details['text'][sequence_number]) or re.match(four_digit_scratch_code_regex, details['text'][sequence_number]):
+        print('from the boxes block image', details['text'][sequence_number], details['conf'][sequence_number])
+        (x, y, w, h) = (details['left'][sequence_number], details['top'][sequence_number], \
+                        details['width'][sequence_number],  details['height'][sequence_number])
+        threshold_img = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # if int(details['conf'][sequence_number]) >= int(preprocessed_details['conf'][sequence_number]):
+    # else: 
+    #     if re.match(date_regs, preprocessed_details['text'][sequence_number]) or re.match(barcode_regex, preprocessed_details['text'][sequence_number]) or re.match(three_digit_scratch_code_regex, preprocessed_details['text'][sequence_number]) or re.match(four_digit_scratch_code_regex, preprocessed_details['text'][sequence_number]):
+    #         print('from the boxes block image', preprocessed_details['text'][sequence_number], preprocessed_details['conf'][sequence_number])
+    #         (x, y, w, h) = (preprocessed_details['left'][sequence_number], preprocessed_details['top'][sequence_number], \
+    #                         preprocessed_details['width'][sequence_number],  preprocessed_details['height'][sequence_number])
+    #         threshold_img = cv2.rectangle(preprocessed_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+parse_text = []
+scratch_code = ''
+for word in details['text']:
+    if re.match(three_digit_scratch_code_regex, word) or re.match(four_digit_scratch_code_regex, word):
+        print('from the append block scrath code', word)
+        scratch_code += word
+    elif re.match(barcode_regex, word):
+        print('from the append block barcode', word)
+        parse_text.append({ 'barcode': word })
+    elif re.match(date_regs, word):
+        print('from the append block date', word)
+        parse_text.append({ 'expiry date': word })
+    # if int(details['conf'][details['text'].index(word)]) >= int(preprocessed_details['conf'][details['text'].index(word)]): 
+    # else:
+    #     print('preprocessed word is higher in conf', word)
+    #     word = preprocessed_details['text'][details['text'].index(word)]
+    #     if re.match(three_digit_scratch_code_regex, word) or re.match(four_digit_scratch_code_regex, word):
+    #         print('from the append block scrath code', word)
+    #         scratch_code += word
+    #     elif re.match(barcode_regex, word):
+    #         print('from the append block barcode', word)
+    #         parse_text.append({ 'barcode': word })
+    #     elif re.match(date_regs, word):
+    #         print('from the append block date', word)
+    #         parse_text.append({ 'expiry date': word })
+parse_text.append({ 'scratch code': scratch_code })
+
+with open('output.txt', 'w') as outfile:
+    json.dump(parse_text, outfile)
+        
+print('parse text', parse_text)
+plt.imshow(image)
+plt.show()
---
+++ keras.py
@@ -0,0 +1,40 @@
+import cv2, re, json, pytesseract
+from pytesseract import Output
+
+date_regs = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
+barcode_regex = '^\d{12}$'
+three_digit_scratch_code_regex = '^\d{3}$'
+four_digit_scratch_code_regex = '^\d{4}$'
+
+img = cv2.imread('test.jpeg')
+config = ('-l --oem 3 --psm 6')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+ret, threshimg = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18)) 
+dilation = cv2.dilate(threshimg, rect_kernel, iterations = 1) 
+img_contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,  cv2.CHAIN_APPROX_NONE)
+
+for cnt in img_contours:
+    x, y, w, h = cv2.boundingRect(cnt)  
+    rect = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+    cropped_img = img[y:y + h, x:x + w] 
+    text = pytesseract.image_to_data(cropped_img, output_type=Output.DICT)
+
+parse_text = []
+scratch_code = ''
+
+for word in text['text']:
+    if re.match(three_digit_scratch_code_regex, word) or re.match(four_digit_scratch_code_regex, word):
+        # print('from the append block scrath code', word)
+        scratch_code += word
+    elif re.match(barcode_regex, word):
+        # print('from the append block barcode', word)
+        parse_text.append({ 'barcode': word })
+    elif re.match(date_regs, word):
+        # print('from the append block date', word)
+        parse_text.append({ 'expiry date': word })
+parse_text.append({ 'scratch code': scratch_code })
+
+with open('output.txt', 'w') as outfile:
+    json.dump(parse_text, outfile)
