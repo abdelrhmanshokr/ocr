ff632bff309ec478714cf5000dd7f60b5cf52aa4
abdelrhmanshokr <abdelrhmanshokr13@gmail.com>
2021-06-21 16:31:44 +0200

    added code to crop the card out of the image then applying the same algorithm to extract scratch code, barcode and date from the cropped image

--- resize_and_rotate.py
+++ resize_and_rotate.py
@@ -1,13 +1,12 @@
-import cv2, re, json, pytesseract
-from pytesseract import Output
-import numpy as np
+import numpy as np 
+import cv2 
 import matplotlib.pyplot as plt 
 
-# first crop the card from the given image 
 MIN_MATCH_COUNT = 4
+
 # (1) prepare data
 template = cv2.imread('test.jpeg')
-image_to_match = cv2.imread('test.jpeg')
+image_to_match = cv2.imread('three.jpeg')
 template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(image_to_match, cv2.COLOR_BGR2GRAY)
 
@@ -63,42 +62,3 @@ cv2.imwrite("matched.jpeg", matched)
 cv2.imwrite("found.jpeg", found)
 cv2.imshow("found", found)
 cv2.waitKey(0)
-
-# then applying the same algorithm on the found image
-date_regs = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
-barcode_regex = '^\d{12}$'
-three_digit_scratch_code_regex = '^\d{3}$'
-four_digit_scratch_code_regex = '^\d{4}$'
-
-img = cv2.imread('found.jpeg')
-custom_config = r'--oem 3 --psm 11'
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-ret, threshimg = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18)) 
-dilation = cv2.dilate(threshimg, rect_kernel, iterations = 1) 
-img_contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,  cv2.CHAIN_APPROX_NONE)
-
-for cnt in img_contours:
-    x, y, w, h = cv2.boundingRect(cnt)  
-    rect = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2) 
-    cropped_img = img[y:y + h, x:x + w] 
-    text = pytesseract.image_to_data(cropped_img, output_type=Output.DICT, config=custom_config)
-
-parse_text = []
-scratch_code = ''
-
-for word in text['text']:
-    if re.match(three_digit_scratch_code_regex, word) or re.match(four_digit_scratch_code_regex, word):
-        print('from the append block scrath code', word)
-        scratch_code += word
-    elif re.match(barcode_regex, word):
-        print('from the append block barcode', word)
-        parse_text.append({ 'barcode': word })
-    elif re.match(date_regs, word):
-        print('from the append block date', word)
-        parse_text.append({ 'expiry date': word })
-parse_text.append({ 'scratch code': scratch_code })
-
-with open('output.txt', 'w') as outfile:
-    json.dump(parse_text, outfile)
