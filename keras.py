c007b97275d3338ed5d4c82b7bd559fe63aba36d
abdelrhmanshokr <abdelrhmanshokr13@gmail.com>
2021-06-23 15:58:32 +0200

    added code that crops the card out of the image using its border and another code to sharpen the image and gray scale it to make it easier to extract all the data from the image, so now this code can extract data from a given image using one of three methods 1)extracts the data from the image without cropping it (if the image is already in proper size) 2)extracts the data from the image after cropping it (if the image isn't in proper size 3)first it's given three parts of the image a part (rectangle) for every part of the data then gray scale it and sharpen it with the proper kernel then extracts data from every part of it (note that this code doesn't extract these rectangles on its own not yet) ..... finally this code works on one image (which should be the template for any image to come so this code could function as it's expected

--- keras.py
+++ keras.py
@@ -1,83 +1,87 @@
 import cv2, re, json, pytesseract
 from pytesseract import Output
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-# first crop the card from the given image 
-MIN_MATCH_COUNT = 4
-# (1) prepare data
-template = cv2.imread('test.jpeg')
-image_to_match = cv2.imread('test.jpeg')
-template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-gray2 = cv2.cvtColor(image_to_match, cv2.COLOR_BGR2GRAY)
+# generatets sharpened image from which text can be extracted if needed
+# image = cv2.imread('all_images.jpeg')
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+# sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
+# thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-# (2) Create SIFT object
-sift = cv2.xfeatures2d.SIFT_create()
+# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+# close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
+# result = 255 - close
 
-# (3) Create flann matcher
-matcher = cv2.FlannBasedMatcher(dict(algorithm = 1, trees = 5), {})
-
-# (4) Detect keypoints and compute keypointer descriptors
-kpts1, descs1 = sift.detectAndCompute(template_gray,None)
-kpts2, descs2 = sift.detectAndCompute(gray2,None)
-
-# (5) knnMatch to get Top2
-matches = matcher.knnMatch(descs1, descs2, 2)
-
-# Sort by their distance.
-matches = sorted(matches, key = lambda x:x[0].distance)
-
-# (6) Ratio test, to get good matches.
-good = [m1 for (m1, m2) in matches if m1.distance < 0.7 * m2.distance]
-
-canvas = image_to_match.copy()
-
-## (7) find homography matrix
-if len(good)>MIN_MATCH_COUNT:
-    # (queryIndex for the small object, trainIndex for the scene )
-    src_pts = np.float32([ kpts1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kpts2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-    # find homography matrix in cv2.RANSAC using good match points
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-    # matchesMask2 = mask.ravel().tolist()
-    h,w = template.shape[:2]
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv2.perspectiveTransform(pts,M)
-    cv2.polylines(canvas,[np.int32(dst)],True,(0,255,0),3, cv2.LINE_AA)
-else:
-    print( "Not enough matches are found - {}/{}".format(len(good),MIN_MATCH_COUNT))
+# cv2.imwrite('sharpen.jpeg', sharpen)
+# cv2.imwrite('thresh.jpeg', thresh)
+# cv2.imwrite('close.jpeg', close)
+# cv2.imwrite('result.jpeg', result)
+# cv2.imshow('sharpen', sharpen)
+# cv2.imshow('thresh', thresh)
+# cv2.imshow('close', close)
+# cv2.imshow('sharpen.jpeg', sharpen)
+# cv2.waitKey(0)
 
 
-# (8) drawMatches
-matched = cv2.drawMatches(template,kpts1,canvas,kpts2,good,None)#,**draw_params)
 
-# (9) Crop the matched region from scene
-h,w = template.shape[:2]
-pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-dst = cv2.perspectiveTransform(pts,M)
-perspectiveM = cv2.getPerspectiveTransform(np.float32(dst),pts)
-found = cv2.warpPerspective(image_to_match,perspectiveM,(w,h))
+# first crop the card out of the given image 
+imageSrc = cv2.imread('sharpen.jpeg')
 
-# (10) save and display
-cv2.imwrite("matched.jpeg", matched)
-cv2.imwrite("found.jpeg", found)
-cv2.imshow("found", found)
+# First cut the source down slightly
+h = imageSrc.shape[0]
+w = imageSrc.shape[1]
+cropInitial = 10
+imageSrc = imageSrc[100:50+(h-cropInitial*2), 50:50+(w-cropInitial*2)]
+
+# Threshold the image and find edges (to reduce the amount of pixels to count)
+ret, imageDest = cv2.threshold(imageSrc, 180, 255, cv2.THRESH_BINARY_INV)
+imageDest = cv2.Canny(imageDest, 100, 100, 3)
+
+# Create a list of remaining pixels
+points = cv2.findNonZero(imageDest)
+
+# Calculate a bounding rectangle for these points
+hull = cv2.convexHull(points)
+x,y,w,h = cv2.boundingRect(hull)
+
+# Crop the original image to the bounding rectangle
+imageResult = imageSrc[y:y+h,x:x+w]
+cv2.imwrite('cropped_image.jpeg', imageResult)
+cv2.imshow('cropped_image.jpeg', imageResult)
 cv2.waitKey(0)
 
-# then applying the same algorithm on the found image
+
+
+
+
+# then applying the same algorithm on the found image to extract scratch code, barcode and date
 date_regs = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
 barcode_regex = '^\d{12}$'
 three_digit_scratch_code_regex = '^\d{3}$'
 four_digit_scratch_code_regex = '^\d{4}$'
 
-img = cv2.imread('found.jpeg')
-custom_config = r'--oem 3 --psm 11'
+img = cv2.imread('test.jpeg')
+# custom_config = r'--oem 3 --psm 8' # works with the cropped a sharpened image where each part is cropped on its own
+# custom_config = r'--oem 3 --psm 6' # works with the whole image after cropping it using the upove code   
+custom_config = r'--oem 3 --psm 12' # works with the whole image without cropping it  
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 ret, threshimg = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
 rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18)) 
-dilation = cv2.dilate(threshimg, rect_kernel, iterations = 1) 
+dilation = cv2.dilate(threshimg, rect_kernel, iterations = 10)
 img_contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,  cv2.CHAIN_APPROX_NONE)
+
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(
+    blur, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    area = cv2.contourArea(c)
+    if area < 5000:
+        cv2.drawContours(thresh, [c], -1, (0, 0, 0), -1)
 
 for cnt in img_contours:
     x, y, w, h = cv2.boundingRect(cnt)  
@@ -87,10 +91,10 @@ for cnt in img_contours:
 
 parse_text = []
 scratch_code = ''
-
+print(text['text'])
 for word in text['text']:
     if re.match(three_digit_scratch_code_regex, word) or re.match(four_digit_scratch_code_regex, word):
-        print('from the append block scrath code', word)
+        print('from the append block scratch code', word)
         scratch_code += word
     elif re.match(barcode_regex, word):
         print('from the append block barcode', word)
--- resize_and_rotate.py
+++ resize_and_rotate.py
@@ -2,63 +2,28 @@ import numpy as np
 import cv2 
 import matplotlib.pyplot as plt 
 
-MIN_MATCH_COUNT = 4
 
-# (1) prepare data
-template = cv2.imread('test.jpeg')
-image_to_match = cv2.imread('three.jpeg')
-template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-gray2 = cv2.cvtColor(image_to_match, cv2.COLOR_BGR2GRAY)
+imageSrc = cv2.imread('test.jpeg')
 
-# (2) Create SIFT object
-sift = cv2.xfeatures2d.SIFT_create()
+# First cut the source down slightly
+h = imageSrc.shape[0]
+w = imageSrc.shape[1]
+cropInitial = 10
+imageSrc = imageSrc[100:50+(h-cropInitial*2), 50:50+(w-cropInitial*2)]
 
-# (3) Create flann matcher
-matcher = cv2.FlannBasedMatcher(dict(algorithm = 1, trees = 5), {})
+# Threshold the image and find edges (to reduce the amount of pixels to count)
+ret, imageDest = cv2.threshold(imageSrc, 180, 255, cv2.THRESH_BINARY_INV)
+imageDest = cv2.Canny(imageDest, 100, 100, 3)
 
-# (4) Detect keypoints and compute keypointer descriptors
-kpts1, descs1 = sift.detectAndCompute(template_gray,None)
-kpts2, descs2 = sift.detectAndCompute(gray2,None)
+# Create a list of remaining pixels
+points = cv2.findNonZero(imageDest)
 
-# (5) knnMatch to get Top2
-matches = matcher.knnMatch(descs1, descs2, 2)
+# Calculate a bounding rectangle for these points
+hull = cv2.convexHull(points)
+x,y,w,h = cv2.boundingRect(hull)
 
-# Sort by their distance.
-matches = sorted(matches, key = lambda x:x[0].distance)
-
-# (6) Ratio test, to get good matches.
-good = [m1 for (m1, m2) in matches if m1.distance < 0.7 * m2.distance]
-
-canvas = image_to_match.copy()
-
-## (7) find homography matrix
-if len(good)>MIN_MATCH_COUNT:
-    # (queryIndex for the small object, trainIndex for the scene )
-    src_pts = np.float32([ kpts1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kpts2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-    # find homography matrix in cv2.RANSAC using good match points
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-    # matchesMask2 = mask.ravel().tolist()
-    h,w = template.shape[:2]
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv2.perspectiveTransform(pts,M)
-    cv2.polylines(canvas,[np.int32(dst)],True,(0,255,0),3, cv2.LINE_AA)
-else:
-    print( "Not enough matches are found - {}/{}".format(len(good),MIN_MATCH_COUNT))
-
-
-# (8) drawMatches
-matched = cv2.drawMatches(template,kpts1,canvas,kpts2,good,None)#,**draw_params)
-
-# (9) Crop the matched region from scene
-h,w = template.shape[:2]
-pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-dst = cv2.perspectiveTransform(pts,M)
-perspectiveM = cv2.getPerspectiveTransform(np.float32(dst),pts)
-found = cv2.warpPerspective(image_to_match,perspectiveM,(w,h))
-
-# (10) save and display
-cv2.imwrite("matched.jpeg", matched)
-cv2.imwrite("found.jpeg", found)
-cv2.imshow("found", found)
+# Crop the original image to the bounding rectangle
+imageResult = imageSrc[y:y+h,x:x+w]
+cv2.imwrite('cropped_result.jpeg', imageResult)
+cv2.imshow('imageResult.jpeg', imageResult)
 cv2.waitKey(0)
